c7e4f7a009a3747f54df68daec49882ca877415f
Kishor <itskishorhere@gmail.com>
2018-11-29 13:54:20 +0530

    Successfully Loaded Static Resources

---
+++ dress2impress/src/main/java/com/amplesoftech/dress2impress/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.amplesoftech.dress2impress.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index()
+	{
+	ModelAndView mv=new ModelAndView("page");
+	mv.addObject("greeting","Welcome to Dress2Impress");
+	return mv;
+	}
+
+}
